a11a94a52b8c7b66ba3dc44bcbaeea9baea78836
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-14 03:02:40 +0900

    関数の型を定義

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-function ButtonComponent(props:any) {
+
+type Props = {
+  clickMethod: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+function ButtonComponent(props:Props) {
   // Reactのイベントの型（Button用）
   // とりあえず、イベント引数の方はこんな感じ
   const click = (event: React.MouseEvent<HTMLButtonElement>) => {
