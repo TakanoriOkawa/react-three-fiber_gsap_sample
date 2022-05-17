e8c96aeda3f805c0d64369288d73710c6bb8e7ad
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-17 23:54:16 +0900

    Cameraコンポーネントを追加

---
+++ src/components/three/Camera.tsx
@@ -0,0 +1,15 @@
+import * as THREE from 'three'
+import React, {useRef} from 'react'
+import {useThree, useFrame} from '@react-three/fiber';
+
+function Camera(props:any) {
+  // 子コンポーネントでカメラデータを親から渡したステート変数を入れる
+  const { camera } = useThree()
+  props.setCamera(camera)
+  // NOTE:Canvasの子にはdivは入れてはダメ。
+  return (
+    <></>
+  )
+}
+
+export default Camera
