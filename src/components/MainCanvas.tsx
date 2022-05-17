1758f1db3d7efa56ff92a6369ac3f0a1aad975b4
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-17 23:54:48 +0900

    ファイル整理

--- src/components/MainCanvas.tsx
+++ src/components/MainCanvas.tsx
@@ -1,66 +1,51 @@
 import * as THREE from 'three'
-import React, { useEffect, useRef } from 'react'
-import {Canvas, useThree, useFrame} from '@react-three/fiber';
+import React, { useState,useEffect, useRef } from 'react'
+import {Canvas} from '@react-three/fiber';
 import gsap from 'gsap';
-
-let _camera: any = ""
-
-// グローバル変数
+import BoxGeometry from './three/BoxGeometry';
+import Camera from './three/Camera';
+import Button from './three/Button';
+import { viewsPoint } from '../module/cameraPoint';
 
 function MainCanvas() {  
+  // set関数を渡す？
+  const [camera, setCamera] = useState();
+
+  const checkCameraState = () =>{
+    console.log(camera);
+  }
+
+  const changeCameraView = (position:string) =>{
+    console.log("位置を変える");
+  }
+
   return (
     <>
-    {/* <Canvas
-      camera={{
-        position: [0, -1.5, 3],
-        fov: 50,
-        aspect: window.innerWidth / window.innerHeight,
-        near: 0.1,
-        far: 2000
-    }}
-    > */}
     <Canvas>
-      <mesh>
-        <boxGeometry args={[2,2,2]}></boxGeometry>
-      </mesh>
-      <Rig></Rig>
+      <BoxGeometry></BoxGeometry>
+      <Camera setCamera={setCamera}></Camera>
     </Canvas>
-    <Button></Button>
+    <Button position='left' changeCameraView={changeCameraView}></Button>
     </>
   )
 }
 
-// childrenはPropsか。受け取り方を勉強しないとな
-// {}の時とProps:anyの時とがとがう？？
-// これは子とってきているのではん？
-function Rig() {
-  const ref = useRef(null);
-  const { camera, mouse } = useThree()
-  _camera = camera;
-  console.log(camera);
-  return (
-    // こちらは単純にコンポーネント内部にあるDOMを設定する
-    <group ref={ref}></group>
-  )
-}
-
 // これはbutton要素なので、canvasの中には置けない。
-function Button() {
-  const anima = () => {
-    console.log("星⭐️" ,_camera)
-    gsap.to(_camera.position, {
-      duration: 2,
-      y:4,
-      z:10,
-      ease: "power3.inOut",
-      onComplete: () => {
-        console.log("end!!!!!!!!!")
-      }
-    })
-  }
-  return (
-    <button onClick={anima}>アニメーション</button>
-  )
-}
+// function Button() {
+//   const anima = () => {
+//     // gsap.to(_camera.position, {
+//     //   duration: 2,
+//     //   y:4,
+//     //   z:10,
+//     //   ease: "power3.inOut",
+//     //   onComplete: () => {
+//     //     console.log("end!!!!!!!!!")
+//     //   }
+//     // })
+//   }
+//   return (
+//     <button onClick={anima}>アニメーション</button>
+//   )
+// }
 
 export default MainCanvas
