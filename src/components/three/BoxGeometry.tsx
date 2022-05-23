e4ae880732fcaad8311984714f500122407d8d45
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-24 00:52:51 +0900

    BoxGeometryにuseFrameアニメーション

--- src/components/three/BoxGeometry.tsx
+++ src/components/three/BoxGeometry.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { Mesh } from 'three'
 
 function BoxGeometry() {
+  const myMesh = useRef<Mesh>(null);  
+  useFrame(() => {
+    if(!myMesh.current) return;
+    myMesh.current.rotation.x += 0.01;
+  })
+  
   return (
-    <mesh>
+    <mesh ref={ myMesh }>
       <boxGeometry args={[2,2,2]}></boxGeometry>
     </mesh>
   )
