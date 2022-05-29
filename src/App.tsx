7dff2a2663e675cd58932ad52207a8ba5a6a7e7d
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-30 00:30:06 +0900

    不要コード削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
 import './App.css';
-import ButtonComponent from './components/ButtonComponent';
-import InputComponent from './components/InputComponent';
 import MainCanvas from './components/MainCanvas';
-import Parent from './components/test/Parent';
 
 function App() {
   return (
