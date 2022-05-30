741ba567623a2d6aa8d05c9d16f68b254869725c
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-31 00:32:05 +0900

    scssテスト

---
+++ src/page/SecondPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../scss/second-page.scss';
+
+function SecondPage() {
+  return (
+    <div className='second-page'>
+      <h1 className="title">SecondPage</h1>
+
+    </div>
+  )
+}
+
+
+export default SecondPage
