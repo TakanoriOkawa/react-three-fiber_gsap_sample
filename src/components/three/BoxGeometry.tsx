677592c2fea7db0dbc839f5101043d9dbf539666
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-30 23:52:44 +0900

    ホバーでカラー変更機能

--- src/components/three/BoxGeometry.tsx
+++ src/components/three/BoxGeometry.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { Mesh, Vector3Tuple } from 'three'
 
@@ -8,14 +8,23 @@ type Props  = {
 
 function BoxGeometry(props: Props) {
   const myMesh = useRef<Mesh>(null);  
+  const [hovered, setHovered] = useState(false);
+
   useFrame(() => {
     if(!myMesh.current) return;
     myMesh.current.rotation.x += 0.01;
   })
 
   return (
-    <mesh {...props} ref={ myMesh } scale={[1, 0.4, 1]}>
-      <boxGeometry></boxGeometry>
+    <mesh 
+      {...props} 
+      ref={ myMesh } 
+      scale={[1, 0.4, 1]}
+      onPointerOver={() => {setHovered(true)}}
+      onPointerOut={() => {setHovered(false)}}
+    >
+      <boxGeometry />
+      <meshStandardMaterial color={ hovered ? "hotpink" : "orange"} />
     </mesh>
   )
 }
