d253ee504eeac34c0ccb8ced42ee8487766d7334
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-06-01 16:03:45 +0900

    Groupのコンポーネント分割

--- src/components/second/MainCanvas.tsx
+++ src/components/second/MainCanvas.tsx
@@ -1,25 +1,60 @@
-import React, { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Vector3Tuple, Group } from 'three';
+import { Vector3Tuple } from 'three';
 import * as THREE from 'three';
+import { config, animated, useSpring } from '@react-spring/three';
 
+interface GeometryType {
+  position: Vector3Tuple,
+  color: string,
+};
+interface Props {
+  geometriesData: GeometryType[]
+}
 
-function Box2() {
+interface receiveProps {
+  geometryData: GeometryType
+}
+
+function Box(props: receiveProps) {
+  const [clicked, setClicked] = useState(false);
+  const geometryData = props.geometryData
+  // const { scale } = useSpring({})
+  return (
+    <mesh 
+      position={geometryData.position}
+      scale={clicked ? 2 : 1}
+      onClick={() => {setClicked(!clicked)}}
+    >
+      <boxGeometry></boxGeometry>
+      <meshStandardMaterial color={geometryData.color}></meshStandardMaterial>
+    </mesh>
+  )
+}
+
+// useFrameは子でないと使えないので、グループコンポーネント作成
+function BoxGroup(props: Props) {
   const BoxGroup = useRef<THREE.Group>(null);
-
   useFrame(() => {
     if(!BoxGroup.current) return;
     BoxGroup.current.rotation.y += 0.01;
     BoxGroup.current.rotation.z += 0.02;
   })
+  // ここの渡し方をもっと深く知りたい
+  const geometriesData  = props.geometriesData;
+  return (
+    <group ref={BoxGroup}>
+      { geometriesData.map((geometryData, index) => (
+        <Box key={index} geometryData={geometryData}></Box>
+      ))}
+    </group>
+  )
+}
 
 
-  interface GeometryType {
-    position: Vector3Tuple,
-    color: string,
-  };
 
-  const GeometryInfo:GeometryType[] = [
+function MainCanvas() {
+  const geometriesData:GeometryType[] = [
     {
       color: 'orange',
       position: [-2,-1,0]
@@ -33,23 +68,9 @@ function Box2() {
       position: [0,-1,0],
     }
   ]
-
-  return (
-    <group ref={BoxGroup}>
-      { GeometryInfo.map((value,index) => (
-        <mesh key={index} position={value.position}>
-          <boxGeometry></boxGeometry>
-          <meshStandardMaterial color={value.color}></meshStandardMaterial>
-        </mesh>
-      ))}
-    </group>
-  )
-}
-
-function MainCanvas() {
   return (
     <Canvas>
-      <Box2></Box2>
+      <BoxGroup geometriesData={geometriesData}></BoxGroup>
       <pointLight power={40} position={[0,10,0]}></pointLight>
       <ambientLight />
     </Canvas>
